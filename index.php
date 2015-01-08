3df9f3bd77e53c399e0ddb3bbb3994855ca2beb3
Michel GILLET <michel@avalone-fr.com>
2015-01-08 17:30:54 +0100

    Positionnements des éléments du header Styles et mise en place page d'accueil Menus

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>AVALONE, développements Web et Appli à Pessac</title>
+		<meta name="description" content="AVALONE, société informatique spécialisée dans le développement d'applications web et applicatives" />
+		<meta name="author" content="Michel GILLET" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<link rel="shortcut icon" href="favicon.ico" />
+		<link rel="stylesheet" href="includes/styles/styles.css" />
+	</head>
+	<body>
+		<div>
+			<header>
+				<div>
+					<a id="logo" href="index.php">
+						<p class="top"><span class="orange">AVA</span><span class="standard">LONE</span></p>
+						<p class="bottom">Solutions de développement Web &amp; Applicatives</p>	
+					</a>
+					<div id="search">
+						<form id="search-box" method="get" action="" name="search">
+							<div class="query">
+								<input type="text" value="Rechercher..." name="q" onclick="this.value=''">
+							</div>
+							<div class="submit">
+								<input type="submit" alt="Rechercher" value=" ">
+							</div>
+						</form>
+					</div>
+					<div id="phonenumber">
+						<div class="imgphone">&nbsp;</div>
+						<div class="imgnumber">&nbsp;</div>
+					</div>
+				</div>
+			</header>
+			<nav>
+				<ul>
+					<li>
+						<a href="index.html">Accueil</a>
+					</li>
+					<li>
+						<a href="">La société</a>
+						<ul>
+							<li><a href="">Qui sommes nous</a></li>
+							<li><a href="">Nos valeurs</a></li>
+							<li><a href="">Nous partenaires</a></li>
+							<li><a href="">Nous contacter</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="">Les applis</a>
+						<ul>
+							<li><a href="">Logiciels sur mesure</a></li>
+							<li><a href="">Langages utilisés</a></li>
+							<li><a href="">Bases de données</a></li>
+							<li><a href="">Applications métiers</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="">Le web</a>
+						<ul>
+							<li><a href="">Langages utilisés</a></li>
+							<li><a href="">Les supports média</a></li>
+							<li><a href="">Hébergement</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="">Le réseau</a>
+						<ul>
+							<li><a href="">Techonologies utilisées</a></li>
+							<li><a href="">Les systèmes</a></li>
+							<li><a href="">La maintenance</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="">Contact</a>
+						<ul>
+							<li><a href="">Nous contacter</a></li>
+							<li><a href="">Offres</a></li>
+							<li><a href="">Candidatures</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+			<div class="content_body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie egestas urna, ac egestas leo laoreet at. Mauris ac orci elit. Mauris ut erat sodales, semper nisl vel, elementum sapien. Praesent turpis justo, tempor in hendrerit vitae, feugiat vel eros. Integer id enim nulla. Suspendisse placerat gravida justo, et faucibus risus dapibus sed. Etiam quis lectus nec ante dignissim dictum et ut nisl. Aliquam et urna efficitur, dapibus nulla eu, faucibus ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin enim lacus, fringilla id volutpat non, lacinia vel tellus. Proin eget pulvinar tortor. Pellentesque ut mollis justo.
+					Fusce dapibus lacus justo, eget volutpat orci tincidunt vitae. Fusce posuere lorem nec purus vestibulum sagittis. Pellentesque efficitur sollicitudin rhoncus. Donec tempus dui sed dolor hendrerit, et rutrum leo ornare. Sed lectus massa, consectetur eget pretium non, tempor ac metus. Curabitur eu laoreet ex. Aenean mattis sapien orci, ac convallis diam aliquet ac. Sed vehicula felis eget leo molestie fringilla. Donec eu justo sed nisi volutpat iaculis.
+					Quisque rhoncus pellentesque egestas. Duis fermentum rutrum elit feugiat scelerisque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris viverra sapien ut urna iaculis, vitae fermentum turpis molestie. Nam dignissim dolor sit amet lacus tempor blandit. Quisque suscipit enim consectetur sodales tempor. Quisque ante neque, laoreet ac ex nec, gravida imperdiet nisi. Donec vestibulum facilisis ex in accumsan. Quisque consectetur et ante eu lobortis. Integer sit amet suscipit mi. Aliquam ante mi, interdum ac tortor sed, dictum dignissim augue. Vestibulum rutrum velit purus, id posuere est placerat a. Nulla venenatis urna ac mauris lacinia finibus. Curabitur hendrerit lobortis mi.
+					Sed sed libero ut augue placerat accumsan eu at neque. Suspendisse non pulvinar sapien, in vehicula tellus. Quisque turpis augue, posuere vel aliquet porta, interdum eget dui. In hendrerit orci lorem, sed laoreet tellus commodo a. Nam sit amet laoreet neque. Aliquam non gravida ante. Aenean malesuada, orci sed eleifend rhoncus, mi odio euismod nunc, sed auctor nisi massa in turpis. Cras posuere tristique massa, et auctor lorem rhoncus ac. Vestibulum id iaculis tellus, sed tempor libero. Donec eget est a lectus ornare faucibus. Maecenas tempus, elit eget scelerisque rhoncus, nibh mauris bibendum lorem, id faucibus elit enim at arcu.
+					Morbi ultrices lacus et ante mollis, hendrerit aliquam orci sodales. Pellentesque vestibulum enim vitae tortor sagittis, sed feugiat dolor sollicitudin. Aliquam molestie rutrum euismod. Ut vestibulum at enim a vehicula. Vestibulum vitae tempus arcu. Donec cursus orci eu lorem hendrerit volutpat. Mauris finibus erat sed mauris sollicitudin semper. Morbi quis vestibulum nisl. Aliquam aliquam diam nec dolor auctor porta. Curabitur ullamcorper, massa ut condimentum dapibus, dolor elit malesuada nibh, a auctor nibh nibh at ex. Suspendisse orci sapien, molestie et dignissim sed, placerat eget eros. Curabitur faucibus mi massa, rhoncus malesuada enim dignissim ut.
+				</p> 
+			</div>
+			<footer class="page_footer">
+				<div class="top">
+					<p>
+						Avalone, crée en 1997 par l'actuel responsable Daniel Porro et installée à Pessac, sur la périphérie de Bordeaux, a su conforter son expérience et son savoir-faire en ingénierie informatique 
+						et sur la gestion des systèmes d'information de ses clients. Définir, concevoir, développer, exploiter, supporter et maintenir des applications web ou des logiciels spécifiques sont les 
+						maitres-mots d'Avalone.La société possède une expertise sur 4 corps de métier: les interfaces web (l'hébergement, et le développement de sites internet, les intranets, les applications pour 
+						smartphone ou tablette, les application métiers); la création de logiciels spécifiques, avec des fonctionnalités sur-mesures, pour un métier précis. 
+						La gestion des base de données du marché telles que PostgrSQL, Oracle, SQL Server, MySQL, Access; et enfin la liaison entre des applications, des sites, des plateformes, des logiciels. 
+						L'équipe est composée d'ingenieurs développeurs, de chefs de projets et de partenaires.
+					</p>
+				</div>
+				<div class="bottom">
+					&copy; Copyright Avalone
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
